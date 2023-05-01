3603ebbc31940cb357437ceaaac59af84974ae22
Bart <bart@internetlab.nl>
2023-05-01 19:37:04 +0100

    added google tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,26 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Bart Fluitsma | Web developer</title>
-        <meta name='description' content='Web development for sustainable businesses. Win over your customers and build a strong brand with a modern website.'/>
+        <meta name='description' content='Web development for sustainable businesses. Win over your customers and build a strong brand with a modern website.' />
         <link rel="icon" href="/icons/favicon-32x32.png" />
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script async
+          src="https://www.googletagmanager.com/gtag/js?id=G-MP1BZY54FV"
+          strategy="afterInteractive" />
+        <Script>
+          {`        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-MP1BZY54FV');`}
+        </Script>
       </Head>
       <Component {...pageProps} />
     </>

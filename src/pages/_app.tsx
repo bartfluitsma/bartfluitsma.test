60845d322f7af7d69a8a250dc5d6ec4c89dfeba9
Bart <bart@internetlab.nl>
2023-05-01 15:38:54 +0100

    site title and meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Bart Fluitsma | Web developer</title>
+        <meta name='description' content='Web development for sustainable businesses. Win over your customers and build a strong brand with a modern website.'/>
+        <link rel="icon" href="/icons/favicon-32x32.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+
 }

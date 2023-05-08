88d5ccb74a25fb86e3e3eb67146159cc297a1a60
Bart <bart@internetlab.nl>
2023-05-08 11:08:25 +0100

    404 page added

---
+++ src/pages/404.tsx
@@ -0,0 +1,25 @@
+import Header from '@/components/Header';
+import Heading from '@/components/Headings';
+import CustomLink from '@/components/LinkIcon';
+
+const Custom404 = () => {
+    return (
+        <>
+            <Header />
+            <div className='mt-20'>
+                <Heading as={1}>
+                    404 - page not found
+                </Heading>
+                <p className="text-lg text-gray-600 mb-8">
+                    Oops! Looks like the page you're looking for doesn't exist.
+                </p>
+                <div className='w-fit'>
+                    <CustomLink name='Visit homepage' linkToWeb='/' icon={false} />
+                </div>
+            </div>
+
+        </>
+    );
+};
+
+export default Custom404;
